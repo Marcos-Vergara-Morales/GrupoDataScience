7ce1acb979d37185c3901c59d64781e85d09755f
Marcos-Vergara-Morales <idu026677@usal.es>
2024-05-27 19:04:58 +0200

    Initial commit

---  DatosPelículas.R
+++  DatosPelículas.R
@@ -1,15 +1,5 @@
 ##Datos Peli
 
-##1r paso.Importar Datos
-
-library(readr)
-reviews_filmaffinity <- read_csv("~/Downloads/reviews_filmaffinity.csv", 
-                                 col_types = cols(`film_name||gender||film_avg_rate||review_rate||review_title||review_text` = col_character()))
-View(reviews_filmaffinity)
-
-
-#2ndo paso. Limpiar datos
-
 #Instalar tidyr
 
 install.packages("tidyr")
@@ -18,26 +8,25 @@ install.packages("tidyr")
 library(readr)
 library(tidyr)
 
-# Leemos el archivo CSV y vemos que limpiar
+##1r paso.Importar Datos
+
 reviews_filmaffinity <- read_csv("~/Downloads/reviews_filmaffinity.csv", 
                                  col_types = cols(`film_name||gender||film_avg_rate||review_rate||review_title||review_text` = col_character()))
+View(reviews_filmaffinity)
 
-# Separar la columna en múltiples columnas
+
+#2ndo paso. Ver datos
+
+# Separar la columna en múltiples columnas, ya que al extraerlos estaban todos en la misma columna
+
 reviews_filmaffinity <- reviews_filmaffinity %>%
   separate(`film_name||gender||film_avg_rate||review_rate||review_title||review_text`, 
            into = c("film_name", "gender", "film_avg_rate", "review_rate", "review_title", "review_text"), 
            sep = "\\|\\|", 
-           remove = TRUE)
+           remove = TRUE) 
 
 # Ver las primeras filas del nuevo dataframe
 head(reviews_filmaffinity)
-
-# Ahora veremos NA
-
-na_datos <- reviews_filmaffinity %>%
-  filter(is.na(film_name) | is.na(gender) | is.na(film_avg_rate) | is.na(review_rate) | is.na(review_title) | is.na(review_text))
-
-print(na_datos) ##vemos que no hay datos que contenga valores NA en la base de datos
 
 #Por último cambiamos el nombre a los datos
 
@@ -45,6 +34,12 @@ Peliculas <- reviews_filmaffinity
 
 print(Peliculas)
 
+summary(Peliculas)
 
+colnames(x = Peliculas)
+
+summary(Peliculas$review_rate)
+
+media_peli <- mean(Peliculas$film_avg_rate)
 
 
