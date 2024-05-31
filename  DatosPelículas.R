6ccbaac2f5a8c862773d935ebdc8709ae42baecb
Marcos-Vergara-Morales <idu026677@usal.es>
2024-05-31 23:38:49 +0200

    Initial commit

---  DatosPelículas.R
+++  DatosPelículas.R
@@ -136,28 +136,218 @@ install.packages("SnowballC")
 install.packages("NLP")
 install.packages("RColorBrewer")
 
+
 library(tm)
 library(wordcloud)
 library(SnowballC)
 library(NLP)
 library(RColorBrewer)
 
-#construimos corpus a partir de la varible review_text
+## Primero vamos a revisar si dentro de nuestra variable review_text hay opiniones vacíos
 
-corpus <- Corpus(VectorSource(Peliculas$review_text))
+# verificamos si hay textos vacíos o nulos en review_text
 
-#ahora con el corpus vamos a "limpiar" todas las opniones
+num_NA <- sum(is.na(Peliculas$review_text))  # Contar NA
 
-corpus <- tm_map(corpus, content_transformer(tolower)) #con esto todas las mayúsculas se transforman en minúsculas
-corpus <- tm_map(corpus, removeNumbers) # eliminamos número de las opiniones
-corpus <- tm_map(corpus, removePunctuation) # eliminacion signos de puntuación
-corpus <- tm_map(corpus, removeWords, stopwords("spanish")) #eliminacion palbra comunes de la lengua castellana como conjunciones o preposiciones, son palabra generales y no aportarian "riqueza"/info a lo que buscamos
+num_EMPTY <- sum(Peliculas$review_text == "")  # Contar cadenas vacías
 
-####el corpues no me sale:/
+# Hacemo recuento con función cat()
+cat("Número de opiniones nulas:", num_NA, "\n")
+cat("Número de opiniones vacías:", num_EMPTY, "\n")
+
+# Aunque no haya opiniones nulas ni vacías eliminamos filas con review_text vacío o nulo
+
+Peliculas <- Peliculas[!(is.na(Peliculas$review_text) | Peliculas$review_text == ""), ]
+
+
+#construimos corpus a partir de la varible review_text(usamos paquete "quanteda" ya que sirve manejar grandes conjuntos de datos)
+
+corpus_todas_pelis <- corpus(Peliculas, text_field = "review_text")
+
+# verificamos el contenido del corpus
+summary(corpus_todas_pelis, 5)  # Resumen de los primeros 5 documentos del corpus
+
+# Crear tokens y aplicar transformaciones
+tokens_todas_pelis <- tokens(corpus_todas_pelis, 
+                             remove_punct = TRUE,   # Eliminar puntuación
+                             remove_numbers = TRUE) %>%  # Eliminar números
+                    tokens_tolower() %>%  # Convertir a minúsculas
+                tokens_remove(stopwords("spanish")) %>%  # Eliminar stopwords
+             tokens_wordstem()  # Aplicar stemming
+
+# Verificar los tokens
+print(tokens_todas_pelis[1:5])  # Mostrar los primeros 5 tokens
+
+# Crear un Document-Feature Matrix (dfm)
+dfm_todas_pelis <- dfm(tokens_todas_pelis)
+
+# Sumar las frecuencias de cada palabra directamente desde el dfm
+words_todas_pelis <- topfeatures(dfm_todas_pelis, n = nfeat(dfm_todas_pelis))
+
+# Convertir a un dataframe
+df_todas_pelis <- data.frame(word = names(words_todas_pelis), freq = words_todas_pelis)
+
+# Verificar el dataframe
+head(df_todas_pelis)  # Mostrar las primeras filas del dataframe
+
+# Generar la nube de palabras
+set.seed(1234)
+wordcloud(words = df_todas_pelis$word, freq = df_todas_pelis$freq, min.freq = 2,
+          max.words = 200, random.order = FALSE, rot.per = 0.35,
+          colors = brewer.pal(8, "Dark2"))
 
 
 
 
+# Convertir el dfm a una matriz
+matrix_todas_pelis <- as.matrix(dfm_todas_pelis)
+
+# Sumar las frecuencias de cada palabra
+words <- sort(colSums(matrix), decreasing = TRUE)
+df <- data.frame(word = names(words), freq = words)
+
+# Verificar el dataframe
+head(df)  # Mostrar las primeras filas del dataframe
+
+# Generar la nube de palabras
+set.seed(1234)
+wordcloud(words = df$word, freq = df$freq, min.freq = 2,
+          max.words = 200, random.order = FALSE, rot.per = 0.35,
+          colors = brewer.pal(8, "Dark2"))
+
+
+## Ahora haremos una nube de palabra de las 3 mejores peliculas valoradas y de las 3 peores valoradas.
+
+# primero ordenamos el dataframe por la media de valoración de forma descendente descendente
+
+medias_por_pelicula_ordenado <- medias_por_pelicula %>%
+  arrange(desc(media_review_rate))
+
+# la pelicula mejor valorada
+head(medias_por_pelicula_ordenado, n=1)
+
+# la pelicula peor valorada
+tail(medias_por_pelicula_ordenado, n=1)
+
+# Mejor pelicula valorada: El Bola(7,52)
+
+#Vamos a realizar nube de datos
+
+# seleccionamos pelicula
+
+nombre_pelicula1 <- "El bola"
+
+# filtramos el data frame solo para obtener las filas de "El bola"
+
+pelicula_elbola <- Peliculas[Peliculas$film_name == nombre_pelicula1, ]
+
+# Verificar el resultado
+
+head(pelicula_elbola)
+
+# creamos el corpus a partir de review_text
+# Crear el corpus usando quanteda
+
+corpus_elbola <- corpus(pelicula_elbola$review_text)
+
+# Verificar el contenido del corpus
+summary(corpus_elbola, 5)  # Resumen de los primeros 5 documentos del corpus
+
+
+# Crear tokens y aplicar transformaciones
+tokens_elbola <- tokens(corpus_elbola, 
+                 remove_punct = TRUE,   # eliminamos signos de puntuación
+                 remove_numbers = TRUE) %>%  # eliminamos números
+  tokens_tolower() %>%  # transformas todo a letras minúsculas
+  tokens_remove(stopwords("spanish")) %>%  # eliminamos las palbras que no aportan valor, aquí ponemos por ejmeplo las conjunciones o preposiciones del español como y, en,etc.. ya que en si no aportan nada de valor a lo que se busca
+  tokens_wordstem()  # reduce la palabra a su propia raiz
+
+# Verificar los tokens
+print(tokens_elbola[1:5])  # mostramos los 5 primeros tokens
+
+# creamos un Document-Feature Matrix (dfm) 
+dfm_elbola <- dfm(tokens_elbola)
+
+# Verificar el dfm
+print(dfm_elbola)
+
+# Sumar las frecuencias de cada palabra
+
+words_elbola <- topfeatures(dfm_elbola, n = nfeat(dfm_elbola))
+
+# Convertir a un dataframe
+
+df_elbola <- data.frame(word = names(words_elbola), freq = words_elbola)
+
+# Verificar el dataframe
+head(df_elbola)  # Mostrar las primeras filas del dataframe
+
+# Generar la nube de palabras
+set.seed(1234)  # Para reproducibilidad
+wordcloud(words = df_elbola$word, freq = df_elbola$freq, min.freq = 2,
+          max.words = 200, random.order = FALSE, rot.per = 0.35,
+          colors = brewer.pal(8, "Dark2"))
+
+
+# Peor película valorada: Torrente(3,52)
+
+#Vamos a realizar nube de datos
+
+# seleccionamos pelicula
+
+nombre_pelicula2 <- "Torrente 3"
+
+# filtramos el data frame solo para obtener las filas de "El bola"
+
+pelicula_torrente <- Peliculas[Peliculas$film_name == nombre_pelicula2, ]
+
+# Verificar el resultado
+
+head(pelicula_torrente)
+
+# creamos el corpus a partir de review_text
+# Crear el corpus usando quanteda
+
+corpus_torrente <- corpus(pelicula_torrente$review_text)
+
+# Verificar el contenido del corpus
+summary(corpus_torrente, 5)  # Resumen de los primeros 5 documentos del corpus
+
+
+# Crear tokens y aplicar transformaciones
+tokens_torrente <- tokens(corpus_torrente, 
+                        remove_punct = TRUE,   # Eliminar puntuación
+                        remove_numbers = TRUE) %>%  # Eliminar números
+  tokens_tolower() %>%  # Convertir a minúsculas
+  tokens_remove(stopwords("spanish")) %>%  # Eliminar stopwords
+  tokens_wordstem()  # Aplicar stemming
+
+# Verificar los tokens
+print(tokens_torrente[1:5])  # Mostrar los primeros 5 tokens
+
+# Crear un Document-Feature Matrix (dfm)
+
+dfm_torrente <- dfm(tokens_torrente)
+
+# Verificar el dfm
+print(dfm_torrente)
+
+# Sumar las frecuencias de cada palabra
+
+words_torrente <- topfeatures(dfm_torrente, n = nfeat(dfm_torrente))
+
+# Convertir a un dataframe
+
+df_torrente <- data.frame(word = names(words_torrente), freq = words_torrente)
+
+# Verificar el dataframe
+head(df_torrente)  # Mostrar las primeras filas del dataframe
+
+# Generar la nube de palabras
+set.seed(1234)  # Para reproducibilidad
+wordcloud(words = df_torrente$word, freq = df_torrente$freq, min.freq = 2,
+          max.words = 200, random.order = FALSE, rot.per = 0.35,
+          colors = brewer.pal(8, "Dark2"))
 
 
 
